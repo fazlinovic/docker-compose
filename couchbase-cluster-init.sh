8ff2a5f275fdd44e9a15f01ef0cb60e64d206b21
fazlinovic <erbe34sadWW>
2021-06-13 13:14:00 +0200

    Added couchbase cluster init script with support for cached docker layers by AWS CodeBuild.

--- couchbase-cluster-init.sh
+++ couchbase-cluster-init.sh
@@ -56,37 +56,37 @@ fi
 
 echo "Cluster not initialized. Initializing..."
 # Install couchbase cluster + bucket
-/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-username=$ADMIN_USER --cluster-password=$ADMIN_PASSWORD --cluster-port=8091 --cluster-index-ramsize=256 --cluster-fts-ramsize=256 --cluster-ramsize=2048 --services=data,index,query,fts
+/opt/couchbase/bin/couchbase-cli cluster-init -c $HOSTNAME:8091 --cluster-username=$ADMIN_USER --cluster-password=$ADMIN_PASSWORD --cluster-port=8091 --cluster-index-ramsize=256 --cluster-fts-ramsize=256 --cluster-ramsize=2048 --services=data,index,query,fts
 sleep 5
 
 # Create bucket
-/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u $ADMIN_USER -p $ADMIN_PASSWORD --wait --bucket=$COUCHBASE_BUCKET --bucket-type=couchbase --bucket-ramsize=256  --bucket-replica=0
+/opt/couchbase/bin/couchbase-cli bucket-create -c $HOSTNAME:8091 -u $ADMIN_USER -p $ADMIN_PASSWORD --wait --bucket=$COUCHBASE_BUCKET --bucket-type=couchbase --bucket-ramsize=256  --bucket-replica=0
 sleep 5
 
 # Create user
-/opt/couchbase/bin/couchbase-cli user-manage -c 127.0.0.1:8091 -u $ADMIN_USER -p $ADMIN_PASSWORD --set --rbac-username $COUCHBASE_DB_USERNAME --rbac-$ADMIN_PASSWORD $COUCHBASE_DB_PASSWORD --roles bucket_full_access[$COUCHBASE_BUCKET] --auth-domain local
+/opt/couchbase/bin/couchbase-cli user-manage -c $HOSTNAME:8091 -u $ADMIN_USER -p $ADMIN_PASSWORD --set --rbac-username $COUCHBASE_DB_USERNAME --rbac-$ADMIN_PASSWORD $COUCHBASE_DB_PASSWORD --roles bucket_full_access[$COUCHBASE_BUCKET] --auth-domain local
 
 # Build primary and secondary indices
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE PRIMARY INDEX ON \`"$COUCHBASE_BUCKET"\` USING GSI;"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE PRIMARY INDEX ON \`"$COUCHBASE_BUCKET"\` USING GSI;"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_creator_index\` ON \`"$COUCHBASE_BUCKET"\`(\`creator\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_creator_index\` ON \`"$COUCHBASE_BUCKET"\`(\`creator\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_composite_index\` ON \`"$COUCHBASE_BUCKET"\`(\`eloquent_type\`,\`team_id\`,\`deleted_at\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_composite_index\` ON \`"$COUCHBASE_BUCKET"\`(\`eloquent_type\`,\`team_id\`,\`deleted_at\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_deleted_at_index\` ON \`"$COUCHBASE_BUCKET"\`(\`deleted_at\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_deleted_at_index\` ON \`"$COUCHBASE_BUCKET"\`(\`deleted_at\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inspection_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`inspection_id\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inspection_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`inspection_id\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_team_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`team_id\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_team_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`team_id\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_document_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`document_type\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_document_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`document_type\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_eloquent_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`eloquent_type\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_eloquent_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`eloquent_type\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`type\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_type_index\` ON \`"$COUCHBASE_BUCKET"\`(\`type\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_city_index\` ON \`"$COUCHBASE_BUCKET"\`(\`city\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_city_index\` ON \`"$COUCHBASE_BUCKET"\`(\`city\`);"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inspection_signature_index\` ON \`"$COUCHBASE_BUCKET"\`(\`signature_inspector\`,\`signature_customer\`) WHERE (\`eloquent_type\` = \"inspections\");"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inspection_signature_index\` ON \`"$COUCHBASE_BUCKET"\`(\`signature_inspector\`,\`signature_customer\`) WHERE (\`eloquent_type\` = \"inspections\");"
 
-/opt/couchbase/bin/cbq -e 127.0.0.1:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inventory_item_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`inventory_item_id\`);"
+/opt/couchbase/bin/cbq -e $HOSTNAME:8093 -u $ADMIN_USER -p $ADMIN_PASSWORD --script "CREATE INDEX \`def_inventory_item_id_index\` ON \`"$COUCHBASE_BUCKET"\`(\`inventory_item_id\`);"
